1bbc644f96c129313da8facef2b2cef2df32fc9e
Oleg A <t0rr@mail.ru>
2022-04-02 12:28:46 +0300

    chore: single server feature

--- ugc/src/core/config.py
+++ ugc/src/core/config.py
@@ -1,5 +1,5 @@
 from logging import config as logging_config
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseSettings, FilePath, SecretStr
 
@@ -26,7 +26,7 @@ class KafkaConfig(BaseSettings):
     class Config:
         env_prefix = "KAFKA_"
 
-    bootstrap_servers: List[str] = ["127.0.0.1"]
+    bootstrap_servers: Union[List[str], str] = "127.0.0.1"
     security_protocol: str = "SASL_SSL"
     sasl_mechanism: str = "SCRAM-SHA-512"
     sasl_plain_username: Optional[str]
