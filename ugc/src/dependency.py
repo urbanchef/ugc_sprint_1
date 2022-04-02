ab8b52d517298a68690817714d73fc67f88a604f
Oleg A <t0rr@mail.ru>
2022-04-02 12:29:00 +0300

    fix: kafka client from cfg

--- ugc/src/dependency.py
+++ ugc/src/dependency.py
@@ -1,15 +1,17 @@
-import asyncio
 from functools import lru_cache
 
 from aiokafka import AIOKafkaProducer
 
-from core import config
+from .core.config import KafkaConfig
 
 
 @lru_cache()
 def get_kafka_producer() -> AIOKafkaProducer:
-    aioproducer = AIOKafkaProducer(
-        bootstrap_servers=f'{config.KAFKA_HOST}:{config.KAFKA_PORT}'
+    cfg = KafkaConfig()
+    return AIOKafkaProducer(
+        bootstrap_servers=cfg.bootstrap_servers,
+        security_protocol=cfg.security_protocol,
+        sasl_mechanism=cfg.sasl_mechanism,
+        sasl_plain_username=cfg.sasl_plain_username,
+        sasl_plain_password=cfg.sasl_plain_password.get_secret_value(),
     )
-
-    return aioproducer
