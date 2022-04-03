72998ed61bf46330deba68539fdbc0a3e4edf248
Roman Mushchinskiy <roman.mushchinskiy@gmail.com>
2022-04-03 23:17:39 +0300

    flake8 string length fix

--- ugc/tests/src/test_main_test.py
+++ ugc/tests/src/test_main_test.py
@@ -9,7 +9,7 @@ client = TestClient(app)
 
 def test_track_movie_progress():
     headers = {
-        "Authorization": """Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.\
+        "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.\
         eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0ODkzMDUzMSwianRpIjoiNTlkYjUzMzUtY\
         2Q2Zi00Mjk4LTg3MDctZDFhZTE2YWQzYTM3IiwidHlwZSI6ImFjY2VzcyIsInVzZXJ\
         fdXVpZCI6IjY4ZWZlYzBkLTZiMzktNDU1ZC1hNDFjLTU4NDljMDI2OTU0YiIsIm5iZi\
@@ -17,8 +17,7 @@ def test_track_movie_progress():
         E5My0xOTg1LTQ2N2YtYTNlMC01NTliMjJlZDNmODkiLCJ1c2VybmFtZSI6IlRlc3RfVX\
         NlciIsImVtYWlsIjoidXNlckBtYWlsLmNvbSIsImlzX3N1cGVydXNlciI6ZmFsc2UsIm\
         NyZWF0ZWRfYXQiOiIyMDIyLTA0LTAyVDIwOjAxOjM1LjQ4MjU3MCIsInJvbGVzIjpbIn\
-        N1YnNjcmliZXIiXX0.qvItA7hrVd1KPyJKWR_MqTYNL2cPK3ZuPuBdCVZLae0
-        """
+        N1YnNjcmliZXIiXX0.qvItA7hrVd1KPyJKWR_MqTYNL2cPK3ZuPuBdCVZLae0"
     }
     movie_id = "d50728e2-4f3e-4070-a5ba-6c3de400a9a4"
     topic_name = "views"
