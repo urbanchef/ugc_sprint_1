5eaf18d279b67c3a943f6d585cf5b527c6c6cd39
Oleg A <t0rr@mail.ru>
2022-04-11 02:07:22 +0300

    chore: add JSON serializer

--- ugc/src/dependency.py
+++ ugc/src/dependency.py
@@ -1,13 +1,30 @@
 import logging
 import ssl
+from datetime import datetime
 from functools import lru_cache
+from json import dumps
 from typing import Any, Dict
+from uuid import UUID
 
 from aiokafka import AIOKafkaProducer
 
 from .core.config import JWTConfig, KafkaConfig
 
 logger = logging.getLogger(__name__)
+
+
+def serializer(value: Dict[str, Any]) -> bytes:
+    """Convert a Dictionary to JSON Bytes String."""
+    value = value.copy()
+
+    for k, v in value.items():
+        if isinstance(v, UUID):
+            value[k] = str(v)
+
+        if isinstance(v, datetime):
+            value[k] = v.strftime("%Y-%m-%d %H:%M:%S")
+
+    return dumps(value).encode()
 
 
 @lru_cache()
@@ -17,6 +34,7 @@ def get_kafka_producer() -> AIOKafkaProducer:
         bootstrap_servers=cfg.bootstrap_servers,
         security_protocol=cfg.security_protocol,
         sasl_mechanism=cfg.sasl_mechanism,
+        value_serializer=serializer,
     )
     if cfg.sasl_plain_username:
         params["sasl_plain_username"] = cfg.sasl_plain_username
