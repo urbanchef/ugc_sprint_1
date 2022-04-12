43b6d366e14aecbe324f8cd8a4547b89764ecec9
Oleg A <t0rr@mail.ru>
2022-04-12 10:31:05 +0300

    chore: add views migration

--- etl/migrations/views.sql
+++ etl/migrations/views.sql
@@ -1,26 +1,26 @@
-CREATE TABLE likes (
+CREATE TABLE views (
     user_uuid UUID,
     movie_uuid UUID,
     datetime DateTime,
-    liked UInt8
+    progress UInt32
 ) Engine = MergeTree
 PARTITION BY toYYYYMM(datetime)
 ORDER BY (movie_uuid, user_uuid, datetime);
 
 
-CREATE TABLE likes_queue (
+CREATE TABLE views_queue (
     user_uuid UUID,
     movie_uuid UUID,
     datetime DateTime,
-    liked UInt8
+    progress UInt32
 )
 ENGINE = Kafka
 SETTINGS    kafka_broker_list = 'rc1b-5902ancqg160diig.mdb.yandexcloud.net:9091',
-            kafka_topic_list = 'likes',
-            kafka_group_name = 'clickhouse-likes-group',
+            kafka_topic_list = 'views',
+            kafka_group_name = 'clickhouse-views-group',
             kafka_format = 'JSONEachRow';
 
 
-CREATE MATERIALIZED VIEW likes_queue_mv TO likes AS
-    SELECT user_uuid, movie_uuid, datetime, liked
-    FROM likes_queue;
+CREATE MATERIALIZED VIEW views_queue_mv TO views AS
+    SELECT user_uuid, movie_uuid, datetime, progress
+    FROM views_queue;
