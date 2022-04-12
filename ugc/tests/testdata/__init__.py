acf255a34f49b7e35f4787847f6c068a3d9ac2e9
Oleg A <t0rr@mail.ru>
2022-04-12 13:22:08 +0300

    refactor: split tests

--- ugc/tests/testdata/__init__.py
+++ ugc/tests/testdata/__init__.py
@@ -1,2 +1,15 @@
 MOVIE_ID = "d50728e2-4f3e-4070-a5ba-6c3de400a9a4"
 SECONDS_WATCHED = 5025
+HEADERS = {
+    "Content-Type": "application/json",
+    "Authorization": (
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9"
+        ".eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTY0ODkzMDUzMSwianRpIjoiNTlkYjUzMzUtY2Q2Zi00Mjk4L"
+        "Tg3MDctZDFhZTE2YWQzYTM3IiwidHlwZSI6ImFjY2VzcyIsInVzZXJfdXVpZCI6IjY4ZWZlYzBkLTZ"
+        "iMzktNDU1ZC1hNDFjLTU4NDljMDI2OTU0YiIsIm5iZiI6MTY0ODkzMDUzMSwiZXhwIjoxNzQzNTM4N"
+        "TMxLCJyZWZyZXNoX3V1aWQiOiJiNGMwM2E5My0xOTg1LTQ2N2YtYTNlMC01NTliMjJlZDNmODkiLCJ"
+        "1c2VybmFtZSI6IlRlc3RfVXNlciIsImVtYWlsIjoidXNlckBtYWlsLmNvbSIsImlzX3N1cGVydXNlc"
+        "iI6ZmFsc2UsImNyZWF0ZWRfYXQiOiIyMDIyLTA0LTAyVDIwOjAxOjM1LjQ4MjU3MCIsInJvbGVzIjp"
+        "bInN1YnNjcmliZXIiXX0.qvItA7hrVd1KPyJKWR_MqTYNL2cPK3ZuPuBdCVZLae0"
+    ),
+}
