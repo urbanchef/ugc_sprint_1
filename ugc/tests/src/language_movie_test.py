77d1d1a65dd8f4830fd66898383cadcb99b1ba9b
Vasilii Safronov <vasi1988@mail.ru>
2022-04-12 17:28:05 +0300

    Added language movies test

---
+++ ugc/tests/src/language_movie_test.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+import pytest
+
+from ugc.src.schemas import LanguageMovie
+
+from ..testdata import HEADERS, MOVIE_ID
+
+pytestmark = pytest.mark.asyncio
+
+
+class TestLanguageMovie:
+    """Presents a test of adding a movie language."""
+
+    url = f"/movies/{MOVIE_ID}/watched"
+
+    async def test_success(self, make_post_request):
+        """Test success case."""
+        data = LanguageMovie(language_movie="Ru-ru")
+        response = await make_post_request(
+            self.url,
+            headers=HEADERS,
+            data=data.json(),
+        )
+        assert response.status == HTTPStatus.OK
