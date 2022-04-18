f24338dc8a22a39448e77a1c3827ee32dd0a3317
Vasilii Safronov <vasi1988@mail.ru>
2022-04-19 01:59:36 +0300

    added TODO

--- ugc/src/schemas/rating.py
+++ ugc/src/schemas/rating.py
@@ -5,4 +5,5 @@ from ugc.src.schemas.base import OrjsonBaseModel
 
 class RatingMessage(OrjsonBaseModel):
     rating: int
+    # TODO: datetime реализовать через фабрику Field(default_factory=dt.now)
     datetime: dt = dt.now()
